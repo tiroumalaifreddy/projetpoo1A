97b5a14bdfeea2cab62eaf29cb8c94a8fcbb43d2
Diallo <physiquedure@gmail.com>
2021-04-21 18:16:45 +0200

    la classe moyenne glissante

--- __main__.py
+++ __main__.py
@@ -3,6 +3,7 @@ from transformers.transformers import Transformers
 from transformers.conversion_csv import Conversion_CSV as cv
 from transformers.conversion_json import Conversion_json as cj
 from transformers.jointure_vertical import Jointure_vertical
+from transformers.moyenne_glissante import Moyenne_glissante as mo
 
 if __name__ == "__main__":
     estimators = Estimators()
@@ -10,13 +11,12 @@ if __name__ == "__main__":
     estimators.fit()
     transformers.transform()
 
-c_csv=cv('D:/Perso/ENSAI/', "covid-hospit-incid-reg-2021-03-03-17h20.csv")
-
+c_csv=cv('/Users/Mamadoudiallo/Downloads/', "test.csv")
 data_un = c_csv.conversion_en_csv()
 data_deux = c_csv.conversion_en_csv()
-
+test=mo.moyenne_glissante(data_un,10)
 J = Jointure_vertical(data_un, data_deux)
-print(*J.fusion_v(), sep='\n')
+#print(*test, sep='\n')
 
 
 #c_json=cj('D:/Perso/ENSAI/', "VacancesScolaires.json")
