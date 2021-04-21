22d9d9cfd01af2fd54a2e412331e062ddd0c3bc8
Freddy Tiroumalai <ftiroumala@gmail.com>
2021-04-21 19:37:55 +0200

    exemple d'une selection de deux variables

--- __main__freddy.py
+++ __main__freddy.py
@@ -2,11 +2,7 @@ from estimators.estimators import Estimators
 from transformers.transformers import Transformers
 from transformers.conversion import Conversion
 from transformers.jointure_vertical import Jointure_vertical
-<<<<<<< HEAD
 from transformers.selection_variable import Selection_variable
-=======
-from transformers.moyenne_glissante import Moyenne_glissante as mo
->>>>>>> 79e733a51a283e742b2890835c139df25ec94eb8
 
 if __name__ == "__main__":
     estimators = Estimators()
@@ -14,14 +10,6 @@ if __name__ == "__main__":
     estimators.fit()
     transformers.transform()
 
-<<<<<<< HEAD
-c_csv=cv('/Users/Mamadoudiallo/Downloads/', "test.csv")
-data_un = c_csv.conversion_en_csv()
-data_deux = c_csv.conversion_en_csv()
-test=mo.moyenne_glissante(data_un,10)
-J = Jointure_vertical(data_un, data_deux)
-#print(*test, sep='\n')
-=======
 donnees_brut = Conversion('D:/Perso/ENSAI/', "covid-hospit-incid-reg-2021-03-03-17h20.csv")
 
 data_un = donnees_brut.convert()
@@ -31,18 +19,13 @@ data_un = donnees_brut.convert()
 #
 # print(*J.fusion_v(), sep = "\n")
 
-selection = Selection_variable(data_un, "nomReg")
-print(selection.select_var())
+selection_une = Selection_variable(data_un, "nomReg")
+selection_deux = Selection_variable(data_un, "numReg")
+var_un = selection_une.select_une_var()
+var_deux = selection_deux.select_une_var()
+J_deux = Jointure_vertical(var_un, var_deux)
+jointure = J_deux.fusion_v()
+
+print(*jointure, sep="\n")
 
 
-
-
-
->>>>>>> 9fbfaa155488aa811fc24128a794200ce8c8eeee
-
-
-
-<<<<<<< HEAD
-#print(c_json.conversion_en_json())
-=======
->>>>>>> 9fbfaa155488aa811fc24128a794200ce8c8eeee
--- transformers/selection_variable.py
+++ transformers/selection_variable.py
@@ -3,5 +3,14 @@ class Selection_variable():
         self.data = data
         self.variable = variable
 
-    def select_var(self):
-        return [row[self.data[0].index(self.variable)] for row in self.data]
+    def select_une_var(self):
+        return [[row[self.data[0].index(self.variable)]] for row in self.data]
+
+    # def select_multiple_var(self):
+    #     new_data = [[row[self.data[0].index(self.liste_variable[0])]] for row in self.data]
+    #     for variable in self.liste_variable[1:]:
+    #         for i in range(len(new_data)):
+    #             new_data[i].append(self.select_une_var(variable)[i])
+    #     return new_data
+
+
